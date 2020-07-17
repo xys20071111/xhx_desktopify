32ef5b50590d4f5828c2aaf09e0797965218dba5
xys20071111 <xys20071111@163.com>
2020-07-17 13:11:48 +0800

    complete

---
+++ index.js
@@ -0,0 +1,39 @@
+const URL_PREFIX = 'http://hot-xhx.61.com';
+
+const mime = require('mime')
+const buffer = require('buffer');
+const fs = require('fs');
+const http = require('http');
+const app = require('express')();
+app.get('/js/*',(req,res)=>{
+	http.get(`${URL_PREFIX}${req.path}`,r=>{
+		if(r.statusCode !== 200){
+			r.resume();
+			throw new Error(`Cannot get:${URL_PREFIX}${req.path}`);
+		}
+		let src = ''
+		r.on('data',d=>{src += d});
+		r.on('end',()=>{
+			src = src.replace(new RegExp('channel.channelConfig.channelId','gm'),'channel.channelEumn.IOS');
+			src = src.replace(`url = clientCore.GlobalConfig.isApp ? "js/" + packName + ".js" : "js/" + packName + ".js?" + Math.random();`,`url = clientCore.GlobalConfig.isApp ? "js/" + packName + ".js" : "js/" + packName + ".js";`)
+			res.send(src);
+			console.log(`GET ${URL_PREFIX}${req.path}`);
+		});
+	});
+});
+app.all('/index.html',(req,res)=>{
+        res.set('Content-type',mime.getType('index.html'));
+        res.send(fs.readFileSync('./index.html'));
+});
+app.all('/*',(req,res)=>{
+	http.get(`${URL_PREFIX}${req.path}`,r=>{
+		let chunks = [];
+		r.on('data',chunk=>{chunks.push(chunk)});
+		r.on('end',()=>{
+			console.log(`GET ${URL_PREFIX}${req.path}`);
+			res.set('content-type',mime.getType('req.path'))
+			res.send(buffer.Buffer.concat(chunks))
+		})
+	});
+});
+app.listen(8080)
